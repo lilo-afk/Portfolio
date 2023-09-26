d6af8cc879bb8b3678b29d911dc98c35673b75af
lilo-afk <mma.murilo@gmail.com>
2023-09-26 12:33:39 -0300

    :construction: CI: config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import HomeSection from "./components/HomeSection";
 import About from "./components/About";
 import Footer from "./components/Footer";
